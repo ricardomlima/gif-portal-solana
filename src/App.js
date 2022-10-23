270fea3b40295954a014a8b24aaf48049edc4da4
Ricardo M <ricardolima89@gmail.com>
2022-10-23 19:09:05 -0300

    checking if wallet is connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import twitterLogo from "./assets/twitter-logo.svg"
 import "./App.css"
+import { useEffect } from "react"
 
 // Constants
-const TWITTER_HANDLE = "web3dev_"
+const TWITTER_HANDLE = "ricardomlima89"
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  /*
+   * This function defines if the wallet is connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          /*
+           * The solana object gives us a function that allows us to connect
+           * directly with the users wallet
+           */
+          const response = await solana.connect({ onlyIfTrusted: true })
+          console.log(
+            "Connect with public key: !",
+            response.publicKey.toString()
+          );
+        }
+      } else {
+        console.log("Solana object not found!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    /*
+     * Solana team suggests that we should wait for the window to load completely
+     * before checking if wallet exists
+     */
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad)
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
