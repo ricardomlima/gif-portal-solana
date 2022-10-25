e9d77f36a3831e274f9aca76510cb7d52a9746dd
Ricardo M <ricardolima89@gmail.com>
2022-10-24 21:08:50 -0300

    managing git list state from input

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ const TEST_GIFS = [
 const App = () => {
 
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
   /*
    * This function defines if the wallet is connected or not
    */
@@ -59,6 +61,21 @@ const App = () => {
     }
   };
 
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  }
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link:", inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue("");
+    } else {
+      console.log("Empty input. Try again.");
+    }
+  }
+
   /*
    * Show this when user is not connected yet
    */
@@ -71,8 +88,24 @@ const App = () => {
 
   const renderConnectedContainer = () => (
     <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter your gif link here!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          Send
+        </button>
+      </form>
       <div className="gif-grid">
-        {TEST_GIFS.map((gif) => (
+        {gifList.map((gif) => (
           <div className="gif-item" key={gif}>
             <img src={gif} alt={gif} />
           </div>
@@ -92,6 +125,15 @@ const App = () => {
     window.addEventListener("load", onLoad);
     return () => window.removeEventListener("load", onLoad)
   }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Obtaining GIF list...");
+
+      // Define the state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
 
   return (
     <div className="App">
